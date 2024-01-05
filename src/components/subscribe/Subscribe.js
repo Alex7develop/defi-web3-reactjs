79b9dc2452bc9a905d1e1a6998b3639e1c2b7a6f
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2024-01-05 15:56:56 +0300

    All Done

---
+++ src/components/subscribe/Subscribe.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Subscribe.css'
+
+const Subscribe = () => {
+    return (
+        <div className='subscribe'>
+            <div className="content">
+                <h2>Join Our DeFi Community</h2>
+                <form action="https://getform.io/f/3ecab17a-148e-45a6-a147-8483ffcc9d22" method="POST">
+                    <div className="form-container display-col">
+                        <input type="email" name='email' placeholder='Enter your email' />
+                        <button className="btn">Sign Up</button>
+                    </div>
+                    <div className="form-container">
+                        <input type="checkbox"/> <p>Yes, I agree to receive email communications from DeFi.</p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Subscribe 
